33c9aec1fcf6f3e81a10d46f2e9a945b077cbc9b
David E. Barrera <dbarrerap@live.com>
2023-11-30 23:37:16 -0500

    CRUD de Contribuyente

---
+++ app/Http/Controllers/Api/ContribuyenteController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Services\Api\ContribuyenteService;
+use App\Trait\RestResponse;
+use Illuminate\Http\Request;
+
+class ContribuyenteController extends Controller
+{
+    use RestResponse;
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        $filter = $request->filter ?? [];
+        $perPage = $request->paginate['perPage'] ?? null;
+        $page = $request->paginate['page'] ?? 1;
+
+        $service = new ContribuyenteService();
+        $response = $service->getContribuyentes($filter, $perPage, $page);
+        return $this->okResponse($response);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        try {
+            $service = new ContribuyenteService();
+            $response = $service->setContribuyente($request->contribuyente);
+            //
+            return $this->createdResponse($response);
+        } catch (\Throwable $th) {
+            return $this->badRequestResponse($request->contribuyente, $th->getMessage());
+        }
+        
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        try {
+            $service = new ContribuyenteService();
+            $response = $service->getContribuyente($id);
+            //
+            return $this->createdResponse($response);
+        } catch (\Throwable $th) {
+            return $this->badRequestResponse($id, $th->getMessage());
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        try {
+            $service = new ContribuyenteService();
+            $response = $service->updateContribuyente($id, $request->contribuyente);
+            //
+            return $this->createdResponse($response);
+        } catch (\Throwable $th) {
+            return $this->badRequestResponse($id, $th->getMessage());
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        try {
+            $service = new ContribuyenteService();
+            $service->deleteContribuyente($id);
+            //
+            return $this->noContentResponse();
+        } catch (\Throwable $th) {
+            return $this->badRequestResponse($id, $th->getMessage());
+        }
+    }
+}
---
+++ app/Http/Services/Api/ContribuyenteService.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Services\Api;
+
+use App\Models\Contribuyente;
+
+class ContribuyenteService {
+    function getContribuyentes(array $filter, int|null $perPage, int $page) {
+        $contribuyentes = Contribuyente::query()
+        ->when(
+            isset($filter['razon_social']),
+            fn ($q) => $q->where('razon_social', 'like', '%' . $filter['razon_social'] . '%')
+        );
+
+        return (!is_null($perPage))
+            ? $contribuyentes->paginate($perPage, ['*'], 'page', $page)
+            : $contribuyentes->get();
+    }
+
+    function setContribuyente(array $data) {
+        try {
+            $contribuyente = new Contribuyente();
+            $contribuyente->tipo_documento = $data['tipo_documento'] ?? 'RUC';  // RUC, CEDULA, PASAPORTE
+            $contribuyente->numero_documento = $data['numero_documento'];
+            $contribuyente->razon_social = $data['razon_social'];
+            $contribuyente->nombre_comercial = $data['nombre_comercial'];
+            $contribuyente->correo = $data['correo'];
+            $contribuyente->telefono = $data['telefono'] ?? null;
+            $contribuyente->movil = $data['movil'] ?? null;
+            $contribuyente->direccion = $data['direccion'];
+            //
+            $contribuyente->save();
+            return $contribuyente;
+        } catch (\Throwable $th) {
+            throw new \Exception($th->getMessage());
+        }
+    }
+
+    function getContribuyente(string $id) {
+        try {
+            $contribuyente = Contribuyente::findOrFail($id);
+            //
+            return $contribuyente;
+        } catch (\Throwable $th) {
+            throw new \Exception($th->getMessage());
+        }
+    }
+
+    function updateContribuyente(string $id, array $data) {
+        try {
+            $contribuyente = Contribuyente::findOrFail($id);
+            $contribuyente->numero_documento = $data['numero_documento'];
+            $contribuyente->razon_social = $data['razon_social'];
+            $contribuyente->nombre_comercial = $data['nombre_comercial'];
+            $contribuyente->correo = $data['correo'];
+            $contribuyente->telefono = $data['telefono'] ?? null;
+            $contribuyente->movil = $data['movil'] ?? null;
+            $contribuyente->direccion = $data['direccion'];
+            //
+            $contribuyente->save();
+            return $contribuyente;
+        } catch (\Throwable $th) {
+            throw new \Exception($th->getMessage());
+        }
+    }
+
+    function deleteContribuyente(string $id) {
+        try {
+            $contribuyente = Contribuyente::findOrFail($id);
+            $contribuyente->delete();
+            //
+            return null;
+        } catch (\Throwable $th) {
+            throw new \Exception($th->getMessage());
+        }
+    }
+}
