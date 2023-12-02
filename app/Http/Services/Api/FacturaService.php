76f5b7379ba9cbb18fafc96d0117e6bcbebc7f91
David E. Barrera <dbarrerap@live.com>
2023-12-02 16:47:15 -0500

    Genracion de Clave de Acceso

--- app/Http/Services/Api/FacturaService.php
+++ app/Http/Services/Api/FacturaService.php
@@ -25,6 +25,18 @@ class FacturaService {
             $establecimiento = Establecimiento::findOrFail($data['establecimiento']);
             $punto_emision = PuntoEmision::findOrFail($data['punto_emision']);
             $secuencial = $punto_emision->factura;
+            
+            $cbn_fecha = \Carbon\Carbon::parse($data['fecha']);
+            $clave_acceso = $cbn_fecha->format('dmY');
+            $clave_acceso .= Str::padLeft('1', 2, '0');
+            $clave_acceso .= $contribuyente['numero_documento'];
+            $clave_acceso .= $contribuyente['tipo_ambiente'];
+            $clave_acceso .= $establecimiento['numero'];
+            $clave_acceso .= $punto_emision['numero'];
+            $clave_acceso .= Str::padLeft("$secuencial", 9, '0');
+            $clave_acceso .= '12345678';
+            $clave_acceso .= '1';
+            $clave_acceso .= $this->getMod11Dv($clave_acceso);
     
             $factura = new Factura();
             $factura->contribuyente_id = $contribuyente['_id'];
@@ -32,16 +44,23 @@ class FacturaService {
             $factura->establecimiento = $establecimiento['numero'];
             $factura->punto_emision = $punto_emision['numero'];
             $factura->secuencial = Str::padLeft("$secuencial", 9, '0');
-            $factura->fecha = $data['fecha'];
+            $factura->fecha = $cbn_fecha->format('d/m/Y');
             $factura->numero_documento = $establecimiento['numero'] . '-' . $punto_emision['numero'] . '-' . Str::padLeft("$secuencial", 9, '0');
+            $factura->clave_acceso = $clave_acceso;
             $factura->forma_pago = $data['forma_pago'];
+            $factura->base_ice = $data['base_ice'] ?? 0;
             $factura->base_iva = $data['base_iva'];
             $factura->base_cero = $data['base_cero'];
+            $factura->descuento = $data['descuento'];
+            $factura->ice_valor = $data['ice_valor'] ?? null;
             $factura->iva_valor = $data['iva_valor'];
             $factura->subtotal = $data['subtotal']; // SUM base_iva, base_cero
             $factura->total = $data['total']; // SUM subtotal, iva_valor
             $factura->observaciones = $data['observaciones'] ?? null;
+            $factura->estado = 'E';  // E: Emitida, G: XML generado, F: XML firmado, R: Recibido por el SRI, A: Autorizado por el SRI, X: Enviado por correo al cliente
             $factura->save();
+
+            $punto_emision->increment('factura');
     
             foreach($data['detalles'] as $detalle) {
                 $facturaDet = new FacturaDetalle();
@@ -84,5 +103,23 @@ class FacturaService {
         } catch (\Throwable $th) {
             throw new \Exception($th->getMessage());
         }
+    }
+
+    function getMod11Dv($num)
+    {
+        $digits = str_replace(array('.', ','), array('' . ''), strrev($num));
+        if (!ctype_digit($digits)) return false;
+
+        $sum = 0;
+        $factor = 2;
+        for ($i = 0; $i < strlen($digits); $i++) {
+            $sum += substr($digits, $i, 1) * $factor;
+            if ($factor == 7) $factor = 2;
+            else $factor++;
+        }
+        $dv = 11 - ($sum % 11);
+        if ($dv == 10) return 1;
+        if ($dv == 11) return 0;
+        return $dv;
     }
 }
