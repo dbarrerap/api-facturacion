f9c3384a9b3944bdbdf40af739bc81dd99f855c6
David E. Barrera <dbarrerap@live.com>
2023-12-02 16:47:42 -0500

    Seeder para cliente

--- database/seeders/ClienteSeeder.php
+++ database/seeders/ClienteSeeder.php
@@ -14,7 +14,7 @@ class ClienteSeeder extends Seeder
     public function run(): void
     {
         Cliente::insert([
-            ['contribuyente_id' => null, 'tipo_documento' => 'CEDULA', 'numero_documento' => '9999999999999', 'razon_social' => 'CONSUMIDOR FINAL', 'direccion' => null, 'telefono' => null, 'email' => 'facturacion@dev-studio.tech', 'fecha_nacimiento' => null],
+            ['contribuyente_id' => null, 'tipo_documento' => 'CONSUMIDOR FINAL', 'numero_documento' => '9999999999999', 'razon_social' => 'CONSUMIDOR FINAL', 'direccion' => null, 'telefono' => null, 'email' => 'facturacion@dev-studio.tech', 'fecha_nacimiento' => null],
         ]);
     }
 }
